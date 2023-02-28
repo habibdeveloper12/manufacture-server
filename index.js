afc3eab0e0be7cf9480405206a30457f52d5cfb4
Habib1212 <habibur.rahman@elementzerolabs.com>
2023-02-28 22:00:27 +0600

    for new change

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.json())
 
 
 
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.it6xt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
